f9646c9f913747797f5f73b96957c0650d928be0
Rwenjie <1621317578@qq.com>
2019-12-19 09:49:42 +0800

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+###这是一个基于spring Boot 的网上预约挂号平台
+
+"这是一个Spring Boot项目" 
+"这是一个Spring Boot项目" 
---
+++ src/main/java/nuc/edu/cn/RegistrationSysApplication.java
@@ -0,0 +1,13 @@
+package nuc.edu.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationSysApplication.class, args);
+	}
+
+}
